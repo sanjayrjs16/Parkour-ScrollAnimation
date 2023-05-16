89e502b69f2d796b317c6ddd17930158e8f43440
Sanjay <sanjayrjs16@gmail.com>
2023-05-17 00:41:10 +0530

    Promises added to prefetch images

--- src/App.jsx
+++ src/App.jsx
@@ -4,74 +4,94 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const canvasRef = useRef();
+  const contextRef = useRef();
+  const scrollTopRef = useRef();
   const [showText, setShowText] = useState(false);
-  const canvasRef = useRef(null);
-  const contextRef = useRef(null);
-  const imgRef = useRef(new Image());
-  const scrollTopRef = useRef(0);
   const totalFrames = 148;
   useEffect(() => {
-    contextRef.current = canvasRef.current.getContext("2d");
+    contextRef.current = canvasRef?.current?.getContext("2d");
 
-    const preLoadImages = () => {
-      for (let i = 1; i < totalFrames; i++) {
-        const img = new Image();
-        img.src = `./src/assets/${i.toString().padStart(4, "0")}.jpg`;
-      }
-    };
-    const img = imgRef.current;
-    img.src = `./src/assets/0001.jpg`;
+    const img = new Image();
+    img.src = "./src/assets/0001.jpg";
 
     img.onload = () => {
-      contextRef.current.drawImage(img, 0, 0);
+      canvasRef.current.width = window.innerWidth;
+      canvasRef.current.height = window.innerHeight;
+      contextRef.current.drawImage(
+        img,
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
     };
 
-    const changeImage = (index) => {
-      img.src = `./src/assets/${index.toString().padStart(4, "0")}.jpg`;
-      contextRef.current.drawImage(img, 0, 0);
+    const getImageName = (index) =>
+      `./src/assets/${index.toString().padStart(4, "0")}.jpg`;
+
+    const preloadImages = () => {
+      const imageLoadedPromises = [];
+      for (let i = 1; i < totalFrames; i++) {
+        const img = new Image();
+        const imgLoadPromise = new Promise((resolve, reject) => {
+          img.onload = () => resolve(img);
+          img.onerror = reject("Something is wrong...");
+        });
+        imageLoadedPromises.push(imgLoadPromise);
+        img.src = getImageName(i);
+      }
+      return Promise.all(imageLoadedPromises);
+    };
+
+    const updateImage = (index) => {
+      img.src = getImageName(index);
+      contextRef.current.drawImage(
+        img,
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
     };
 
     const handleScroll = (e) => {
-      // const scrollTop = document.body.scrollTop;
       scrollTopRef.current = document.documentElement.scrollTop;
-      console.log(document.documentElement, e.target);
-      const maxScrollable =
+      const maxScrollDistance =
         document.documentElement.scrollHeight - window.innerHeight;
-      const percentScrolled = scrollTopRef.current / (maxScrollable - 1);
+      const fractionScrolled = scrollTopRef.current / maxScrollDistance;
+
       const imageIndex = Math.min(
         totalFrames,
-        Math.ceil(percentScrolled * totalFrames)
+        Math.ceil(fractionScrolled * totalFrames)
       );
-      if (imageIndex < 130) {
-        setShowText(false);
-      } else {
-        setShowText(true);
-      }
-      // console.log(scrollTopRef.current, percentScrolled, imageIndex);
-      requestAnimationFrame(() => changeImage(imageIndex + 1));
+      if (imageIndex < 130) setShowText(false);
+      else setShowText(true);
+      requestAnimationFrame(() => updateImage(imageIndex));
     };
     window.addEventListener("scroll", handleScroll);
-    preLoadImages();
-    return () => window.removeEventListener("scroll", handleScroll);
+    preloadImages();
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
-
   return (
     <>
       <main id={"main"}>
         <section className={"section-container"}>
-          <div class="hero">
-            <h1 class="hero-text">Wanna Learn Parkour ?</h1>
+          <div className="hero">
+            <h2 className="hero-text">Wanna Learn Parkour ?</h2>
           </div>
         </section>
         <section className={"section-container"}>
-          <div className="canvasContainer">
+          <div className={"canvas-container"}>
             <canvas ref={canvasRef} id="scroll-canvas" />
-            {showText && <p> Parkour !</p>}
+            {showText && <p className={"parkour-text"}>Parkour !</p>}
           </div>
         </section>
         <section className={"section-container"}>
-          <div class="hero">
-            <h1 class="hero-text">Micheal Scott Parkour Company!</h1>
+          <div className="hero">
+            <h1 className="hero-text">Micheal Scott Parkour Company!</h1>
             <h2>Learn from the best</h2>
           </div>
         </section>
